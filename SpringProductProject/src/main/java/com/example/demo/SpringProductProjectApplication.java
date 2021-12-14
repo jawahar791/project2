4370c8ac3588236be45ff614419663f95edcc374
jawahar791 <94907931+jawahar791@users.noreply.github.com>
2021-12-14 12:58:46 +0530

    productdetailsSpringboot

---
+++ SpringProductProject/src/main/java/com/example/demo/SpringProductProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductProjectApplication.class, args);
+	}
+
+}
