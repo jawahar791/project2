4c24f560cd94e086fcfb475cca907790aa3baab4
jawahar791 <94907931+jawahar791@users.noreply.github.com>
2021-12-14 11:28:27 +0530

    springboothello

---
+++ SpringHelloworldExample/src/main/java/com/SpringHelloworldExample/SpringHelloworldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.SpringHelloworldExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloworldExampleApplication.class, args);
+	}
+
+}
---
+++ SpringHelloworldExample/src/test/java/com/SpringHelloworldExample/SpringHelloworldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringHelloworldExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloworldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
